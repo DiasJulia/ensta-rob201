5526d5dd73ab1765e4043cef19eee1e8698522b3
Julia <jedl@cin.ufpe.br>
2026-03-23 20:30:27 +0100

    TP2: partial

--- tp_rob201/control.py
+++ tp_rob201/control.py
@@ -32,8 +32,43 @@ def potential_field_control(lidar, current_pose, goal_pose):
     on initial pose, x forward, y on left)
     """
     # TODO for TP2
+    print("Current pose:", int(current_pose[0]), int(current_pose[1]))
+    print("Goal pose:", int(goal_pose[0]), int(goal_pose[1]))
 
-    command = {"forward": 0,
-               "rotation": 0}
+    # Attractice potential field
+
+    K = 2.0  # attractive potential gain
+
+    distance_to_goal = np.linalg.norm(goal_pose[:2] - current_pose[:2])
+
+    # Tolerance to goal
+    T = 10
+    if distance_to_goal < T:
+        gradient = np.array([0.0, 0.0])
+    else:
+        gradient = K * (goal_pose[:2] - current_pose[:2]) / distance_to_goal
+
+     # Repulsive potential field
+    L = 1.0  # repulsive potential gain
+    d_min = 0.5  # minimum distance to obstacle
+
+    for i in range(len(lidar.get_sensor_values())):
+        sensor_dist = lidar.get_sensor_values()[i]
+        if sensor_dist < d_min and sensor_dist > 0.01: 
+            angle = lidar.get_ray_angles()[i]
+            f_rep = L * (1/sensor_dist - 1/d_min) * np.array([np.cos(angle), np.sin(angle)])
+            gradient += f_rep
+    
+    forward_speed = np.linalg.norm(gradient) * 0.5
+    rotation_speed = np.arctan2(gradient[1], gradient[0]) * 0.5
+    
+    # Clamp values to valid ranges [-1, 1]
+    forward_speed = np.clip(forward_speed, -1.0, 1.0)
+    rotation_speed = np.clip(rotation_speed, -1.0, 1.0)
+    
+    print(f"Gradient: {gradient}, Forward speed: {forward_speed}, Rotation speed: {rotation_speed}")
+
+    command = {"forward": forward_speed,
+               "rotation": rotation_speed}
 
     return command
